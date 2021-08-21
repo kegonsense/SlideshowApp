394d41c0989629f0848f5536570f3227a5afd6ad
kengo.sato <sense.sense.1003@gmail.com>
2021-08-21 18:59:08 +0900

    修正。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -24,19 +24,19 @@ class ViewController: UIViewController {
     // スライドショーさせる画像の配列を宣言
     var imageArray:[UIImage] = [
         //imageArray[0]
-        UIImage(named:"/Users/satoukengo/Desktop/課題/SlideshowApp/SlideshowApp/gazou/a")!,
+        UIImage(named:"gazou/a")!,
         
         //imageArray[1]
-        UIImage(named: "/Users/satoukengo/Desktop/課題/SlideshowApp/SlideshowApp/gazou/b")!,
+        UIImage(named: "gazou/b")!,
         
         //imageArray[2]
-        UIImage(named: "/Users/satoukengo/Desktop/課題/SlideshowApp/SlideshowApp/gazou/c")!
+        UIImage(named: "gazou/c")!
     ]
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = UIImage(named: "/Users/satoukengo/Desktop/課題/SlideshowApp/SlideshowApp/gazou/a")!
+        imageView.image = UIImage(named: "gazou/a")!
         //右辺、変数名でもOK、imageView.image = imageArray[0]
         }
     
